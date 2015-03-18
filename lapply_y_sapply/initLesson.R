ad1fca3d6442b1dc1fc0eb387f3d6cd2027f2d51
“alcideschaux” <alcideschaux@uninorte.edu.py>
2015-03-18 10:37:58 -0300

    Path arreglado

--- lapply_y_sapply/initLesson.R
+++ lapply_y_sapply/initLesson.R
@@ -1,6 +1,6 @@
 # Path to installed lesson
-.lessonpath <- file.path(path.package("swirl"), "Courses", "R_Programming",
-                        "lapply_and_sapply")
+.lessonpath <- file.path(path.package("swirl"), "Courses", "Programacion_en_R",
+                        "lapply_y_sapply")
 # Path to dataset
 .datapath <- file.path(.lessonpath, "flag.data.txt")
 # Load dataset
